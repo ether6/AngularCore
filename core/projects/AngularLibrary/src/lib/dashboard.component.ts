801f5c5ba603b9efe3b6d8e69ae6f412ceda9c9a
Grant Gallagher <gallagher.grant@gmail.com>
2018-12-18 15:28:11 -0800

    adding code

---
+++ core/projects/AngularLibrary/src/lib/dashboard.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dash-dashboard',
+  template: `
+    <p>
+      dashboard works!
+    </p>
+  `,
+  styles: []
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ core/projects/AngularLibrary/src/lib/dashboard.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './dashboard.component';
+
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [
+  ],
+  exports: [DashboardComponent]
+})
+export class DashboardModule { }
